0dc45a042e64b8d627e9a28ceb17fedd9485cce1
okorkov <alex.okorkov@gmail.com>
2021-02-09 13:23:46 -0800

    commit tree 428628b90f2d8c74a45373a37d82b10b1fe5ab05

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards(names, event) {
+  let result = [];
+    for(let i = 0; i < names.length; i++) {
+      result.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+    }
+  return result;
+}
+
+
+function countDown(number) {
+  let counter = 0;
+  while (counter <= number) {
+    console.log(counter)
+    counter ++
+  }
+} 
